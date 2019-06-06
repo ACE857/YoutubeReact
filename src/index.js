8e40dd8feb84f4c3a968af0b3fcdc46e1a4bb21e
Suraj Bisht <srjbsht857@gmail.com>
2019-06-06 21:01:28 +0530

    Simple React Application

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+//cretae a new component this should produce some html
 
-import App from './components/app';
-import reducers from './reducers';
+const API_KEY = "AIzaSyAF8eRLR4WrvygpV0JUGhLyGK0zm32HmHc";
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+const App = () => {
+  return <div>Hi!</div>;    // this is javascript code that is
+                          // that is converted into html code by webpack and bable
+
+}
+
+// take this component and genrate HTML and put it on the page
+ReactDOM.render(<App />, document.querySelector('.container'));
